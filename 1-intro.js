3c8b0e32624adcad77e8608cb01780b9505f1cac
Ulrico Carmona <ulricocarmon@gmail.com>
2021-10-12 13:12:28 +0200

    first commit

---
+++ 1-intro.js
@@ -0,0 +1,8 @@
+const amount = 09;
+
+if (amount < 10) {
+   console.log('small number'); 
+}
+else {
+    console.log('large number');
+}
---
+++ 2-globals.js
@@ -0,0 +1,16 @@
+//GLOBALS _ NO WINDOW !!!!!!!!
+
+// __dirname   - path to current directory
+// __filename  - file name
+// require     - function to use modules (CommonJS)
+// module      - info about current module (file)
+// process     - info abot enviroment where the program is being executed
+
+
+
+
+
+console.log(__dirname);
+setInterval(() => {
+    console.log('hello world')
+}, 1000)
